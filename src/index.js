25e23be41b1f9522ff98895bfc0a8c23de26606f
Luis Araujo <laraujo@cognyhub.com>
2025-04-20 23:01:04 -0300

    add GA removido node_modules

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const port = 3000;
 app.use(express.static('src'));
 
 app.get('/', (req, res) => {
-  const filePath = path.join(__dirname, 'index_full.html');
+  const filePath = path.join(__dirname, 'index_gpt.html');
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
       return res.status(500).send('Erro ao ler o arquivo');
@@ -29,7 +29,7 @@ app.get('/short', (req, res) => {
   });
 });
 
-app.get('/gpt', (req, res) => {
+app.get('/full', (req, res) => {
   const filePath = path.join(__dirname, 'index_gpt.html');
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
