fd8b9da10b1453c4d0308bd84540fe61b4b24b85
Luis Araujo <laraujo@cognyhub.com>
2025-05-05 12:18:32 -0300

    add timeline

--- src/index.js
+++ src/index.js
@@ -40,6 +40,39 @@ app.get('/full', (req, res) => {
   });
 });
 
+app.get('/timeline', (req, res) => {
+  const filePath = path.join(__dirname, 'index_timeline.html');
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).send('Erro ao ler o arquivo');
+    }
+
+    res.send(data);
+  });
+});
+
+app.get('/timeline_V1', (req, res) => {
+  const filePath = path.join(__dirname, 'index_timeline_v1.html');
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).send('Erro ao ler o arquivo');
+    }
+
+    res.send(data);
+  });
+});
+
+app.get('/timeline_V2', (req, res) => {
+  const filePath = path.join(__dirname, 'index_timeline_v2.html');
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).send('Erro ao ler o arquivo');
+    }
+
+    res.send(data);
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
